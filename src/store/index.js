590227586728f4e3e0f6a9143771719566ab4a64
Tomás Dornell <tomasdornell.gtd@gmail.com>
2019-03-15 22:12:21 +0100

    Add Redux store and Thunk middleware
    Add store initializer
    Add root reducer
    Add example action, container and reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,1 +1,18 @@
-console.log("Redux goes here")
+// This file creates the Redux store and returns it
+
+import { createStore, applyMiddleware } from 'redux';
+/*
+    Thunk is a Redux middleware for managing complex synchronous logic
+    and asynchronous calls, useful for communicating with the future API
+*/
+import ReduxThunk from 'redux-thunk';
+
+// Root reducer that combines all the smaller reducers
+import rootReducer from './reducers'
+
+const store = createStore(
+    rootReducer,
+    applyMiddleware(ReduxThunk)
+);
+
+export default store;
