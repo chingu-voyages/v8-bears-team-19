0d996fb426adb11109b5c365e3010b87a2bf6349
Burak Tilek <burak.tilek@gmail.com>
2019-03-17 23:51:18 +0300

    Get a specific recipe endpoint is added

--- api/routes/recipes.js
+++ api/routes/recipes.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
+// Get all recipes endpoint
 router.get('/', function(req, res) {
   // TODO: Change below code with database code
   // Get all recipes
   res.json('Hello from recipes route');
 });
 
+// Post a new recipe endpoint
 router.post('/', function(req, res) {
   // TODO: Change below code with database code
   // Add new recipe
   res.json(req.body);
+});
+
+// Get a specific recipe endpoint
+router.get('/:recipeId', function(req, res) {
+  // TODO: Change below code with database code
+  // Add new recipe
+  res.json(req.params.recipeId);
 });
 
 module.exports = router;
