21ae9c255d7596e8e69b97a5cd6b053d9d5a6d6b
Tomás Dornell <tomasdornell.gtd@gmail.com>
2019-03-17 12:52:37 +0100

    Install 'nodemon'

--- src/server.js
+++ src/server.js
@@ -3,6 +3,15 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('is working!')
+});
+
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/ping', function (req, res) {
@@ -13,4 +22,4 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4000);
