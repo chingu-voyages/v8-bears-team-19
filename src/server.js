0441c7e557fe2c6dd70a85d481e0c9618dad1f76
Tomás Dornell <tomasdornell.gtd@gmail.com>
2019-03-18 00:19:53 +0100

    Prepare 'server.js' for merging

--- src/server.js
+++ src/server.js
@@ -1,8 +1,18 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+
+const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
 const recipeRoutes = require('./api/routes/recipes');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('is working!')
+});
+
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/ping', function (req, res) {
