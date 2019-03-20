ea69130abd30897956ae92c403811a53208dd8d1
Tomás Dornell <tomasdornell.gtd@gmail.com>
2019-03-20 15:07:49 +0100

    Add obligatory semicolon to ESLint config

--- src/backend/locales/en-US.js
+++ src/backend/locales/en-US.js
@@ -1,7 +1,7 @@
 var enUS = {
     // Example Hello
     recipe_hello: 'Hello from recipes route',
-    recipe_not_found: (id) => `Recipe with id \'${id}\' not found`,
+    recipe_not_found: (id) => `Recipe with id '${id}' not found`,
     greet: function(name) { return "Hello " + name; }
   };
   module.exports = enUS;
