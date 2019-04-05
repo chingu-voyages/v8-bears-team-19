9d5251f54b6bc37b857db2be6bad5010630e0435
Tomás Dornell <tomasdornell.gtd@gmail.com>
2019-04-05 14:39:21 +0200

    Merge pull request #22 from chingu-voyages/feature/NewRecipeAPI
    Add 'New Recipe' reducers, action and api calls

--- src/frontend/store/actions/recipe.js
+++ src/frontend/store/actions/recipe.js
@@ -1,87 +1,96 @@
 import {
-    GET_RECIPE,
-    GET_RECIPES,
-    ADD_RECIPE,
-    REMOVE_RECIPE,
-    MODIFY_RECIPE,
-    LIKE_RECIPE,
-    SHARE_RECIPE,
-    FORK_RECIPE,
-} from '../../constants'
+  GET_RECIPE,
+  GET_RECIPES,
+  ADD_RECIPE_SUCCESS,
+  ADD_RECIPE_FAILED,
+  REMOVE_RECIPE,
+  MODIFY_RECIPE,
+  LIKE_RECIPE,
+  SHARE_RECIPE,
+  FORK_RECIPE,
+} from '../../constants';
 
-import * as API from '../../api'
+import * as API from '../../api';
 
 // We structure the action like this so we can make async calls thanks to ReactThunk
 export const getRecipe = (id) => (dispatch) => {
-    const result = API.getRecipe(id);
+  const result = API.getRecipe(id);
+  dispatch({
+    type: GET_RECIPE,
+    payload: result
+  });
+  /* Once we use promises, we can do this:
+
+  API.getRecipe(id).then(res => {
     dispatch({
-        type: GET_RECIPE,
-        payload: result
+      type: GET_RECIPE,
+      payload: res.data()
     });
-    /* Once we use promises, we can do this:
+  })
 
-    API.getRecipe(id).then(res => {
-        dispatch({
-            type: GET_RECIPE,
-            payload: res.data()
-        });
-    })
-
-    */
-}
+  */
+};
 
 export const getRecipes = (ids) => (dispatch) => {
-    const result = API.getRecipes(ids);
-    dispatch({
-        type: GET_RECIPES,
-        payload: result
-    });
-}
+  const result = API.getRecipes(ids);
+  dispatch({
+    type: GET_RECIPES,
+    payload: result
+  });
+};
 
-export const addRecipe = (title) => (dispatch) => {
-    API.addRecipe(title);
-    dispatch({
-        type: ADD_RECIPE,
-        payload: { title }
+export const addRecipe = (title, description, author) => (dispatch) => {
+  API.addRecipe(title, description, author)
+    .then(res => {
+      dispatch({
+        type: ADD_RECIPE_SUCCESS,
+        payload: res
+      });
+    })
+    .catch(err => {
+      dispatch({
+        type: ADD_RECIPE_FAILED,
+        payload: err
+      });
     });
-}
+};
 
 export const removeRecipe = (id) => (dispatch) => {
-    API.removeRecipe(id);
-    dispatch({
-        type: REMOVE_RECIPE,
-        payload: id
-    });
-}
+  API.removeRecipe(id);
+  dispatch({
+    type: REMOVE_RECIPE,
+    payload: id
+  });
+};
 
 export const modifyRecipe = (id, newData) => (dispatch) => {
-    API.modifyRecipe(id, newData);
-    dispatch({
-        type: MODIFY_RECIPE,
-        payload: { id, newData }
-    });
-}
+  API.modifyRecipe(id, newData);
+  dispatch({
+    type: MODIFY_RECIPE,
+    payload: { id, newData }
+  });
+};
 
 export const likeRecipe = (id) => (dispatch) => {
-    API.likeRecipe(id);
-    dispatch({
-        type: LIKE_RECIPE,
-        payload: id
-    });
-}
+  API.likeRecipe(id);
+  dispatch({
+    type: LIKE_RECIPE,
+    payload: id
+  });
+};
 
 export const shareRecipe = (id) => (dispatch) => {
-    API.shareRecipe(id);
-    dispatch({
-        type: SHARE_RECIPE,
-        payload: id
-    });
-}
+  API.shareRecipe(id);
+  dispatch({
+    type: SHARE_RECIPE,
+    payload: id
+  });
+};
 
 export const forkRecipes = (id, newName) => (dispatch) => {
-    API.forkRecipes(id, newName);
-    dispatch({
-        type: FORK_RECIPE,
-        payload: { id, newName }
-    });
-}
+  API.forkRecipes(id, newName);
+  dispatch({
+    type: FORK_RECIPE,
+    payload: { id, newName }
+  });
+};
